0796d7955108d833014a8dc8983d62b90f624bfc
Rs Robin <srrobin33@gmail.com>
2019-09-25 11:24:50 +0600

    career planning map

---
+++ app/src/main/java/edu/daffodil/cdc/CareerPlanningMap.java
@@ -0,0 +1,32 @@
+package edu.daffodil.cdc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class CareerPlanningMap extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_career_planning_map);
+        webView =findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
--- app/src/main/java/edu/daffodil/cdc/MainActivity.java
+++ app/src/main/java/edu/daffodil/cdc/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -80,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                     switch (position){
                         case 0:
                             //career plannig map
+                            startActivity(new Intent(MainActivity.this,CareerPlanningMap.class));
 
                             break;
 
