919c9661445301e0ba172da3f363dfadb310c247
Rs Robin <srrobin33@gmail.com>
2019-09-26 10:33:55 +0600

    webView modification

--- app/src/main/java/edu/daffodil/cdc/CareerPlanningMap.java
+++ app/src/main/java/edu/daffodil/cdc/CareerPlanningMap.java
@@ -2,23 +2,60 @@ package edu.daffodil.cdc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class CareerPlanningMap extends AppCompatActivity {
     WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_planning_map);
+
         webView =findViewById(R.id.webView);
+        progressBar=findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+
+
+
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://studentportal.diu.edu.bd//");
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                progressBar.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+                progressBar.setVisibility(View.GONE);
+
+
+            }
+        });
     }
 
     @Override
