3d8435c60d02ba80a2e93a9c5f49b75879c5b03b
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-09-28 09:30:46 +0600

    Schedule an appoinment activity

--- app/src/main/java/edu/daffodil/cdc/ContactUs.java
+++ app/src/main/java/edu/daffodil/cdc/ContactUs.java
@@ -29,25 +29,37 @@ public class ContactUs extends AppCompatActivity implements View.OnClickListener
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fab_phone:
+                String phone = "88-02-8310864";
+                break;
 
             case R.id.fab_call:
-                actionCall("");
+                actionCall("+8801991195535");
+                break;
+
             case R.id.fab_email:
-                actionEmail("");
+                actionEmail();
+                break;
+
             case R.id.fab_location:
                 actionLocation();
+                break;
         }
     }
 
     private void actionLocation() {
+//        Sobhanbag Rd, Dhaka 1215
+        Uri mapUri = Uri.parse("geo:0,0?q=" + Uri.encode("Sobhanbag Rd, Dhaka 1215"));
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
 
     }
 
-    private void actionEmail(String email) {
+    private void actionEmail() {
 
         Intent intent = new Intent(Intent.ACTION_SEND);
-//        String[] strTo = {"someone@host.com"};
-        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        String[] strTo = {"ncdc@daffodilvarsity.edu.bd"};
+        intent.putExtra(Intent.EXTRA_EMAIL, strTo);
         intent.putExtra(Intent.EXTRA_SUBJECT, "CDC App Contact");
         intent.setType("message/rfc822");
         intent.setPackage("com.google.android.gm");
