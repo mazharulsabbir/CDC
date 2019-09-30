e09a45b2d8f663688992778e647e47b779008554
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-09-30 21:58:46 +0600

    Announcement Adapter + Data initiated. -> Bottom Navigation View Initiated.

--- app/src/main/java/edu/daffodil/cdc/Announcements.java
+++ app/src/main/java/edu/daffodil/cdc/Announcements.java
@@ -1,12 +1,17 @@
 package edu.daffodil.cdc;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.daffodil.cdc.Adapters.AnnounceAdapters;
+import edu.daffodil.cdc.model.AnnouncementHelper;
 
 public class Announcements extends AppCompatActivity {
 
@@ -24,6 +29,14 @@ public class Announcements extends AppCompatActivity {
         }
 
         annoucementRV = findViewById(R.id.announceRV);
+        annoucementRV.setHasFixedSize(true);
+        annoucementRV.setLayoutManager(new LinearLayoutManager(this));
+
+        List<AnnouncementHelper> helpers = new ArrayList<>();
+        helpers.add(new AnnouncementHelper("Title", "Message", "Date"));
+
+        announceAdapters = new AnnounceAdapters(this, helpers);
+        annoucementRV.setAdapter(announceAdapters);
 
     }
 
---
+++ app/src/main/java/edu/daffodil/cdc/model/AnnouncementHelper.java
@@ -0,0 +1,38 @@
+package edu.daffodil.cdc.model;
+
+public class AnnouncementHelper {
+    private String title,message,date;
+
+    public AnnouncementHelper() {
+    }
+
+    public AnnouncementHelper(String title, String message, String date) {
+        this.title = title;
+        this.message = message;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
