f639d10db02d896115086ad3377dd6c9a9e97d62
Mazharul Sabbir <42198761+mazharulsabbir@users.noreply.github.com>
2019-10-02 22:12:21 +0600

    Jobs api link updated.

--- app/src/main/java/edu/daffodil/cdc/resources/JsonPlaceHolderApi.java
+++ app/src/main/java/edu/daffodil/cdc/resources/JsonPlaceHolderApi.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface JsonPlaceHolderApi {
-    String BASE_JOBS_URL = "http://skill.jobs/api/";
+    String BASE_JOBS_URL = "http://test.skill.jobs/api/jobs";
 
     String BASE_ANNOUNCEMENT_API = "http://daffodilvarsity.edu.bd/noticeboard/noticeApi/";
 
